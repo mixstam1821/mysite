f5b3f69c12d29fb24c94cd9ec26fe112713153d0
Michael Stamatis <82336443+mixstam1821@users.noreply.github.com>
2023-08-24 17:25:13 +0300

    Update references.js

--- assets/js/references.js
+++ assets/js/references.js
@@ -36,7 +36,8 @@ const references = [/*
         image2 : "https://avatars.githubusercontent.com/u/59832779?v=4",
         message2 : "John and I worked together during a hackathon and the thing that surprised me the most about him was his knowledge of fundamentals. He offered sincere help to the team throughout the competition. He is an impressive problem solver who is able to address complex issues strategically and confidently.",
   },
-*/];
+*/
+];
 
 AOS.init();
 const fillData = () => {
